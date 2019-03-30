0d67d8832ceb460df2d8e600c83bd8f8f9b6cdf4
mujeex <mujahid.bappai@yahoo.com>
2019-03-30 22:06:24 +0800

    collision detection

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const canvas= document.querySelector('#myCanvas')
 const ctx= canvas.getContext('2d')
 
-let xPosition= canvas.width/2
-let yPosition= canvas.height-30
-const dx=2
-const dy=-2
+let xPosition= canvas.width/2;
+let yPosition= canvas.height-30;
+let dx=2;
+let dy= -2;
+const ballRadius=10;
 
 const drawBall=()=>{
     ctx.beginPath();
-    ctx.arc(xPosition, yPosition, 20, 0, Math.PI*2, false);
+    ctx.arc(xPosition, yPosition, ballRadius, 0, Math.PI*2, false);
     ctx.fillStyle='red';
     ctx.fill();
     ctx.closePath()
@@ -18,8 +19,23 @@ const drawBall=()=>{
 const startGame=()=>{
     ctx.clearRect(0,0,canvas.width,canvas.height)
    drawBall()
+
+ // checking for collision for left and right side
+ if( xPosition + dx < ballRadius || xPosition + dx > canvas.width-ballRadius) {
+    dx = -dx;
+}
+// checking for collision for top and bottom
+   if(yPosition + dy < ballRadius||yPosition+dy>canvas.height-ballRadius) {
+       console.log('fired!')
+    dy = -dy;
+   }
+   //changing the position of the ball each time the canvas repaints
     xPosition+=dx;
     yPosition+=dy
+   
+   requestAnimationFrame(startGame)
 }
 
-setInterval(startGame, 10);
+requestAnimationFrame(startGame)
+
+// console.log(canvas.width,canvas.height)
